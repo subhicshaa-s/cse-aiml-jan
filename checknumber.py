d5b09a1e2149cb87bbdb6287b60f298d70364f02
subhicshaa-s <subhicshaasevandhilingam@gmail.com>
2025-12-08 10:40:31 +0530

    check if the given number is positive, negative, zero

---
+++ checknumber.py
@@ -0,0 +1,7 @@
+t = int(input("Enter a number:"))
+if (t==0):
+    print("The given number is zero")
+elif (t>0):
+    print("The given number is positive")
+else:
+    print("The given number is negative")
