3177363d14f9df61b15c1b76f6c993eace05d439
subhicshaa-s <subhicshaasevandhilingam@gmail.com>
2025-12-08 11:14:15 +0530

    student grade calculator

---
+++ grade.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter ur marks for 100:"))
+if (marks>=90 and marks<=100):
+    print("A GRADE")
+elif (marks>=80 and marks<=89):
+    print("B GRADE")
+elif (marks>=70 and marks<=79):
+    print("C GRADE:")
+elif (marks>=60 and marks<=69):
+    print("D GRADE")
+elif (marks,60):
+    print("FAIL")
+else:
+    print("enter correct mark")
