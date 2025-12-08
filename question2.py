78a1c7599d5e2ce0e71ab86c408d24730c0a0d79
subhicshaa-s <subhicshaasevandhilingam@gmail.com>
2025-12-08 10:32:47 +0530

    get input for variable name, score, department. Get score for 100. Divide 100/10, Print it

---
+++ question2.py
@@ -0,0 +1,7 @@
+name = str(input("NAME:"))
+score = int(input("SCORE FOR 100:"))
+department = str(input("DEPARTMENT:"))
+percent = score/10
+print("Student name:",name)
+print("Department :",department)
+print("Student score",percent, "/10")
